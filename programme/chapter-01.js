4df6086e79c5a7d60a45e4cebf410ba4d345914d
Baliram <heyome9a@gmail.com>
2022-09-08 18:37:14 +0530

    insert element into array

--- programme/chapter-01.js
+++ programme/chapter-01.js
@@ -1,2 +1,22 @@
-console.log('hello world');
-// hello
+// Insert element into array
+
+// With for loop
+let arr = [23, 34, 5, 46, 17, 33, 32, 40]
+
+for (let i = arr.length-1; i >= 0; i--) {
+    if (i >= 4) {
+        arr[i+1] = arr[i]
+        if (i===4){
+            arr[i] = 99
+        }
+    }
+}
+console.log(arr)
+
+
+
+// With javascript functions
+let arr2 = [23, 34, 5, 46, 17, 33, 32, 40]
+
+arr2.splice(3, 0, 66)
+console.log(arr2)
